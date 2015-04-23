8f855323adacccadbb906d8ecc8cf55d5b4d517f
Tomas diaz <under.sen@live.cl>
2015-04-23 14:54:33 -0300

    create templates for Postres

--- js/app/app.js
+++ js/app/app.js
@@ -1,18 +1,4 @@
-(function(){
-	var app = angular.module('dessert',[]);
-
-	app.controller('DessertController', function(){
-		this.products = desserts;
-	});
-
-		var desserts =[{
-			name: 'Canastas de Chocolate',
-			Description:'asdfghrewgegegwe',
-			Reviews:'Tomaaaas',
-		},
-		{
-			name: 'Canastas de vainilla',
-			
-		}];
-
-	})();
+// (function(){
+	var app = angular.module('dessert', ['youtube-embed']);
+// }).call(this);
+	
