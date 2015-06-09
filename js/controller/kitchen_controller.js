fc2ac029b79e2fbfffa075ff0df26f23628ac2fd
Tomas diaz <under.sen@live.cl>
2015-06-09 17:48:53 -0300

    add new bullshit

--- js/controller/kitchen_controller.js
+++ js/controller/kitchen_controller.js
@@ -3,11 +3,13 @@
 	app.controller('KitchenController', function ($scope, $firebaseArray) {
 		var recipe_db = new Firebase("https://cocinarte.firebaseio.com/");
 		$scope.recipes = $firebaseArray(recipe_db);
-		$scope.saveData = function () {
+  		$scope.saveData = function () {
 			$scope.recipes.$add({
 				title: $scope.titleRecipe,
-					description: $scope.descriptionRecipe,
-						youtube: $scope.youtubeRecipe,
+				description: $scope.descriptionRecipe,
+				youtube: $scope.youtubeRecipe,
+				
+				image: $scope.imageRecipe,
 
 			});
 		};
